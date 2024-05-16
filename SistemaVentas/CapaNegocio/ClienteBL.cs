c0092dc6b00b4eae8b78dde8907115572a5b8546
Estudiante <estudiante@UCUXA80122.continental.edu.pe>
2024-05-16 17:07:57 -0500

    Version de la Clase

---
+++ SistemaVentas/CapaNegocio/ClienteBL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient; // Driver de SQL Server
+using System.Data; //Buffer de memorias
+using System.Configuration; // Extraer la cadena de conexion de la CPW (CapaPresentacionWindows)
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class ClienteBL : Interface.ICliente
+    {
+        //Extraer la cadena de conexion
+        private static string cadena = ConfigurationManager.ConnectionStrings["cadena"].ConnectionString;
+
+        public DataTable Listar()
+        {
+            using (SqlConnection conexion = new SqlConnection(cadena))
+            {
+                string consulta = "select * from TCliente";
+                //Llevar la consulta a la base de datos y traer los registros de la tabla
+                SqlDataAdapter adapter = new SqlDataAdapter(consulta, conexion);
+                DataTable tabla = new DataTable();
+                adapter.Fill(tabla);
+                return tabla;
+            }
+        }
+
+        public bool Actualizar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Agregar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable Buscar(string codCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(string codCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ SistemaVentas/CapaNegocio/Interface/ICliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data; //Usar los buffer de memoria
+using CapaEntidad;
+
+namespace CapaNegocio.Interface
+{
+    interface ICliente
+    {
+        //Declarar los metodos de la Clase Cliente de la Capa de Negocios
+        DataTable Listar();
+        bool Agregar(Cliente cliente);
+        bool Eliminar(string codCliente);
+        bool Actualizar(Cliente cliente);
+        DataTable Buscar(string codCliente);
+
+
+    }
+}
